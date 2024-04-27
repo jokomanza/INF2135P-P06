b5bd9c98311eff6f24973f206ef6523dba610cf5
Joko Supriyanto <jokomanza@gmail.com>
2024-04-28 02:55:24 +0700

    Add some sample objects instance of insect class

--- src/assessment/Main.java
+++ src/assessment/Main.java
@@ -1,14 +1,24 @@
 package assessment;
 
-/**
- * Buatlah kelas HewanTakBertulangBelakang dan Serangga, perhatikan hierarki yang ada
- * diawal Praktikum 7 untuk mengetahui kelas mana yang abstrak dan mana yang konkrit dan
- * kelas mana mewarisi kelas mana, kemudian isilah body kelas-kelas tersebut. Tampilkan
- * hasilnya pada kelas Utama!
- */
-
-
 public class Main {
 
-    
+    public static void main(String[] args) {
+        Insects ant = new Insects();
+        ant.setSpecies("Formicidae");
+        System.out.println("Ant");
+        System.out.println("Species: " + ant.getSpecies());
+        System.out.println("Phylum: " + ant.getPhylum());
+
+        Insects bee = new Insects();
+        bee.setSpecies("Apis mellifera");
+        System.out.println("Bee");
+        System.out.println("Species: " + bee.getSpecies());
+        System.out.println("Phylum: " + bee.getPhylum());
+
+        Insects butterfly = new Insects();
+        butterfly.setSpecies("Lepidoptera");
+        System.out.println("Butterfly");
+        System.out.println("Species: " + butterfly.getSpecies());
+        System.out.println("Phylum: " + butterfly.getPhylum());
+    }
 }
